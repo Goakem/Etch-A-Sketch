1e225d185a9c99eb00ba3ded70687c2d84ac57c0
Goakem <195132463+Goakem@users.noreply.github.com>
2026-09-18 04:25:03 +0100

    Fix children div sizing
    makes the generated divs responsive and resize accordingly

--- main.js
+++ main.js
@@ -9,8 +9,24 @@ function createNewDiv () {
             const newDiv = document.createElement("div");
             newDiv.classList.add("smallDiv");
 
-            const newDivHeight = (container.offsetHeight / +userInput);
-            const newDivWidth = (container.offsetWidth / +userInput);
+            const containerTrueHeight = container.offsetHeight - 12;
+            const containerTrueWidth = container.offsetWidth - 12;
+            console.log(`This is containers width and height after the 
+                adding of paddings, Height: ${containerTrueHeight} 
+                & ${containerTrueWidth}`
+            );
+
+            const numberOfFlexGap = userInput - 1;
+            const sizeOfFlexGap = numberOfFlexGap * 5;
+            console.log(`This is the size of the gap 
+                between elements ${sizeOfFlexGap}`
+            );
+            
+            const childrenDivWidth = containerTrueWidth - sizeOfFlexGap;
+            const newDivWidth = (childrenDivWidth / userInput) - 2;
+
+            const childrenDivHeight = containerTrueHeight -sizeOfFlexGap;
+            const newDivHeight = (childrenDivHeight / userInput) - 2;
 
             newDiv.style.width = newDivWidth + "px";
             newDiv.style.height = newDivHeight + "px";
