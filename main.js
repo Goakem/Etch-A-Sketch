2cd48b16e23a6885fd8b9f44bb0eaefebf2c79b9
Goakem <195132463+Goakem@users.noreply.github.com>
2026-09-20 07:45:24 +0100

    Fix crash and update variable name
    this debug via console.log and update variable name that don't
    really relate what they are about
    
    the log on the console also allows to see the flow of the program so
    erors can be spotted

--- main.js
+++ main.js
@@ -3,38 +3,44 @@ const btn = document.querySelector("#btn");
 
 function createNewDiv () {
     let userInput = Number(prompt("Enter a number between 1 and 100:"));
-    console.log(`This is the datatype of userInput within stated constraint ${typeof userInput}`);
+    console.log(`This is the datatype of userInput within stated constraint "${typeof userInput}"`);
+    console.log(`This is the user input ${userInput}`);
 
     if ((userInput < 1) || (userInput > 100) || (userInput === NaN)) {
         userInput = Number(prompt("You must pick a number btw 1 & 100"));
-        console.log(`This is the datatype of userInput outside the scope ${typeof userInput}`);
+        console.log(`This is the datatype of userInput outside the scope "${typeof userInput}"`);
+        console.log(`This is the user input ${userInput}`);
     }
+
+    const containerTrueHeight = container.offsetHeight - 12;
+    const containerTrueWidth = container.offsetWidth - 12;
+    console.log(`True container div size: Height is ${containerTrueHeight}, Width is ${containerTrueWidth}`);
+
+    const numberOfFlexGap = userInput - 1;
+    const sizeOfFlexGap = numberOfFlexGap * 5;
+    console.log(`The total numbers of gap: ${numberOfFlexGap} and size of gap: ${sizeOfFlexGap}`);
+    
+    const shareableContainerWidth = containerTrueWidth - sizeOfFlexGap;
+    const newDivWidth = (shareableContainerWidth / userInput) - 2;
+    console.log(`This is the new shareable width of the container div: ${shareableContainerWidth}`);
+    console.log(`This is the size width of a child div: ${newDivWidth}`);
+
+    const shareableContainerHeight = containerTrueHeight -sizeOfFlexGap;
+    const newDivHeight = (shareableContainerHeight / userInput) - 2;
+    console.log(`This is the new shareable Height of the container div: ${shareableContainerHeight}`);
+    console.log(`This is the size height of a child div: ${newDivHeight}`);
+    console.log(`Calc completed, Yay!`);
 
     for(let i = 0; i < userInput; i++) {
         for(let j = 0; j < userInput; j++) {
             console.log(`I made it into the loop`);
+            console.log(`i: ${i} && j: ${j}`)
             const newDiv = document.createElement("div");
             newDiv.classList.add("smallDiv");
-
-            const containerTrueHeight = container.offsetHeight - 12;
-            const containerTrueWidth = container.offsetWidth - 12;
-            console.log(`True div size: Height is ${containerTrueHeight}, Width is ${containerTrueWidth}`);
-
-            const numberOfFlexGap = userInput - 1;
-            const sizeOfFlexGap = numberOfFlexGap * 5;
-            console.log(`The total numbers of gap: ${numberOfFlexGap} and size of gap: ${sizeOfFlexGap}`);
-            
-            const childrenDivWidth = containerTrueWidth - sizeOfFlexGap;
-            const newDivWidth = (childrenDivWidth / userInput) - 2;
-            console.log(`This is the width of the new div: ${childrenDivWidth}`);
-
-            const childrenDivHeight = containerTrueHeight -sizeOfFlexGap;
-            const newDivHeight = (childrenDivHeight / userInput) - 2;
-            console.log(`This is the Height of the new div: ${childrenDivHeight}`);
+            console.log(`newDiv has been created`);
 
             newDiv.style.width = newDivWidth + "px";
             newDiv.style.height = newDivHeight + "px";
-            console.log(`Calc completed, Yay!`);
 
             container.appendChild(newDiv);
             console.log(`Appended`)
@@ -54,7 +60,7 @@ function createNewDiv () {
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
-        console.log(`removing children is my thing`)
+        console.log(`removing children is my thing`);
     }
 }
 
@@ -63,14 +69,15 @@ btn.addEventListener("click", (e) => {
     console.log(`I have been clicked, leggo!`);
 
     if (clickCount === 1) {
-        console.log(`I am 1`);
+        console.log(`I am click 1`);
         createNewDiv();
         console.log(`Click action 1 completed`);
     } else if (clickCount > 1) {
-        console.log(`Click action ${clickCount} completed`)
         removeAllChildNodes(container);
         console.log(`children removed`);
         createNewDiv();
         console.log(`creating new div like always`);
+        console.log(`Click action ${clickCount} completed`);
+
     }
 });
