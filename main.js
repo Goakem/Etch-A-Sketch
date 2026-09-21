572652fbd87060a0730d83205cd0e5f87ae63668
Goakem <195132463+Goakem@users.noreply.github.com>
2026-09-21 21:48:13 +0100

    Add child div to container at runtime

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
 const container = document.querySelector(".container");
 const btn = document.querySelector("#btn");
 
+let userInput = 16;
+
+const containerTrueHeight = container.offsetHeight - 12;
+const containerTrueWidth = container.offsetWidth - 12;
+
+const numberOfFlexGap = userInput - 1;
+const sizeOfFlexGap = numberOfFlexGap * 5;
+
+const shareableContainerWidth = containerTrueWidth - sizeOfFlexGap;
+const newDivWidth = (shareableContainerWidth / userInput) - 2;
+
+
+const shareableContainerHeight = containerTrueHeight -sizeOfFlexGap;
+const newDivHeight = (shareableContainerHeight / userInput) - 2;
+
+for(let i = 0; i < userInput; i++) {
+        for(let j = 0; j < userInput; j++) {
+            const newDiv = document.createElement("div");
+            newDiv.classList.add("smallDiv");
+
+            newDiv.style.width = newDivWidth + "px";
+            newDiv.style.height = newDivHeight + "px";
+
+            container.appendChild(newDiv);
+
+            newDiv.addEventListener("mouseenter", (e) => {
+                e.target.style.backgroundColor = "blue";
+            });
+        }
+    }
+
 function createNewDiv () {
     let userInput = Number(prompt("Enter a number between 1 and 100:"));
 
@@ -8,18 +39,6 @@ function createNewDiv () {
         userInput = Number(prompt("You must pick a number btw 1 & 100"));
     }
 
-    const containerTrueHeight = container.offsetHeight - 12;
-    const containerTrueWidth = container.offsetWidth - 12;
-
-    const numberOfFlexGap = userInput - 1;
-    const sizeOfFlexGap = numberOfFlexGap * 5;
-    
-    const shareableContainerWidth = containerTrueWidth - sizeOfFlexGap;
-    const newDivWidth = (shareableContainerWidth / userInput) - 2;
-    
-
-    const shareableContainerHeight = containerTrueHeight -sizeOfFlexGap;
-    const newDivHeight = (shareableContainerHeight / userInput) - 2;
 
     for(let i = 0; i < userInput; i++) {
         for(let j = 0; j < userInput; j++) {
@@ -34,9 +53,6 @@ function createNewDiv () {
             newDiv.addEventListener("mouseenter", (e) => {
                 e.target.style.backgroundColor = "blue";
             });
-            newDiv.addEventListener("mouseout", (e) => {
-                e.target.style.backgroundColor = "initial";
-            })
         }
     }
 }
@@ -50,9 +66,7 @@ function removeAllChildNodes(parent) {
 btn.addEventListener("click", (e) => {
     let clickCount = performance.eventCounts.get("click");
 
-    if (clickCount === 1) {
-        createNewDiv();
-    } else if (clickCount > 1) {
+    if (clickCount >= 1) {
         removeAllChildNodes(container);
         createNewDiv();
     }
